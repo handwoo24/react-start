e6c701144abfcffc62c8828e3277fadfd2034f71
handwoo24 <handwoo24@gmail.com>
2025-06-18 00:47:18 +0900

    set for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     tsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
-    tanstackStart(),
+    tanstackStart({ target: "vercel" }),
     tailwindcss(),
   ],
 });
